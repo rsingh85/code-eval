b0d1437ff445fe18acf67e026317076968434ebd
Ravinder Singh <rsingh@ravi-singh.com>
2015-12-14 20:49:42 +0000

    Initial commit.

---
+++ CodeEvalCSharp/ReverseWorlds.Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+/// <summary>
+/// https://www.codeeval.com/open_challenges/8/
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+
+                if (null == line)
+                    continue;
+
+                Console.WriteLine(
+                    string.Join(" ", line.Split(' ').Reverse())
+                );
+            }
+        }
+    }
+}
