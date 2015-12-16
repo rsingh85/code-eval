249ae03b90145a8bbfd4a4297471fe34f339d32b
Ravinder Singh <rsingh@ravi-singh.com>
2015-12-16 19:20:05 +0000

    Multiplication tables exercise.

---
+++ CodeEvalCSharp/MultiplicationTables.Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.MultiplicationTables
+{
+    /// <summary>
+    /// https://www.codeeval.com/open_challenges/23/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.Write("{0,-3} ", i * j);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
