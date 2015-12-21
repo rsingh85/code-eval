dceba1f65384d6b44fd08fd7e817f27b6822f007
Ravinder Singh <rsingh@ravi-singh.com>
2015-12-21 20:18:36 +0000

    Multiplication tables and longest lines exercises.

---
+++ CodeEvalCSharp/LongestLines.Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace CodeEval.LongestLines
+{
+    /// <summary>
+    /// https://www.codeeval.com/open_challenges/2/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = null;
+            int max = 0;
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (null == line)
+                        continue;
+
+                    if (lines == null)
+                    {
+                        lines = new List<string>();
+                        max = int.Parse(line);
+
+                        continue;
+                    }
+
+                    lines.Add(line);
+                }
+            }
+
+            foreach (string line in lines.OrderByDescending(l => l.Length).Take(max))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
--- CodeEvalCSharp/MultiplicationTables.Program.cs
+++ CodeEvalCSharp/MultiplicationTables.Program.cs
@@ -8,13 +8,13 @@ namespace CodeEval.MultiplicationTables
     /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static void DisabledMain(string[] args)
         {
             for (int i = 1; i <= 12; i++)
             {
                 for (int j = 1; j <= 12; j++)
                 {
-                    Console.Write("{0,-3} ", i * j);
+                    Console.Write("{0,3} ", i * j);
                 }
 
                 Console.WriteLine();
